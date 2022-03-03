c9ff53d35efc0afaedc8f39ad1e0665c9d704569
Kyungyun Lee, IOS dev <leeky16498@gmail.com>
2022-03-03 16:01:23 +0000

    Initial Commit

---
+++ CalendarSelf/CalendarSelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarSelfApp.swift
+//  CalendarSelf
+//
+//  Created by Kyungyun Lee on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalendarSelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
